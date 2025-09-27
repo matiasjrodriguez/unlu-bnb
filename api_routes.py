1e739a9afdaead38bcc5a68eca640ff55049196b
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2025-09-26 23:06:47 -0300

    correcciones menores

--- api_routes.py
+++ api_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
-@api_bp.route('/login')
+@api_bp.route('/login', methods=['POST'])
 def login_api():
     data = request.get_json()
     username = data.get('username')
