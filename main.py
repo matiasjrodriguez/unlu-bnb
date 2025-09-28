65b36f345ba80c5406795f56e1c1cf5f845a3c43
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2025-09-28 19:45:48 -0300

    SUPERSECRET

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from api_routes import api_bp
 from template_routes import template_bp
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
-app.secret_key = 'clave secreta XDDDD' # No nos olvidemos de cambiar esto chicos, lo ideal es que sea una variable de entorno para la vps
+load_dotenv()
+app.secret_key = os.getenv("SUPERSECRET") # No nos olvidemos de cambiar esto chicos, lo ideal es que sea una variable de entorno para la vps
 app.config['UPLOAD_FOLDER'] = 'static/img'
 
 app.register_blueprint(api_bp)
