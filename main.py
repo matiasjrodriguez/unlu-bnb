59290020b60f1aa64d63bd4baacb53d0d6e07380
Matías Javier Rodriguez <mellarodriguez29@gmail.com>
2025-09-28 22:36:04 -0300

    hotfix

--- main.py
+++ main.py
@@ -13,6 +13,5 @@ app.register_blueprint(api_bp)
 app.register_blueprint(template_bp)
 
 if __name__ == '__main__':
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host="0.0.0.0", port=port)
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
